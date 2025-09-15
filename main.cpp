7eb02ca8454171e3c8628758f8d7b0cba9ad0044
Jesus <jesuslongarea@gmail.com>
2025-09-14 19:16:17 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include "Maze2.cpp"
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
     //reads through the file
     Maze2 maze( "/Users/jesuslongares/CLionProjects/Maze2/maze3_input.txt");
@@ -14,8 +12,3 @@ int main() {
 
     return 0;
 }
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
